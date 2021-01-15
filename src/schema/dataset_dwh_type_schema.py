573460ab1ad25dce3cb057185a7c9dead6cf8325
Tomáš Schmidl <schmidltomas@gmail.com>
2021-01-15 13:31:30 +0100

    Added BaseSchema to serialize all JSON keys in camel case

--- src/schema/dataset_dwh_type_schema.py
+++ src/schema/dataset_dwh_type_schema.py
@@ -1,8 +1,9 @@
 from src import ma
 from src.model import DatasetDwhType
+from .base_schema import BaseSchema
 
 
-class DatasetDwhTypeSchema(ma.SQLAlchemySchema):
+class DatasetDwhTypeSchema(ma.SQLAlchemySchema, BaseSchema):
 	class Meta:
 		model = DatasetDwhType
 		fields = ('type', 'subtype', 'table', 'primary_key', 'properties')
--- src/schema/dataset_schema.py
+++ src/schema/dataset_schema.py
@@ -1,9 +1,10 @@
 from src import ma
 from src.model import Dataset
 from .dataset_dwh_type_schema import DatasetDwhTypeSchema
+from .base_schema import BaseSchema
 
 
-class DatasetSchema(ma.SQLAlchemySchema):
+class DatasetSchema(ma.SQLAlchemySchema, BaseSchema):
 	class Meta:
 		model = Dataset
 		fields = ('id', 'name', 'title', 'ref')
